1d5f7cb3c77e8c68da6d987ec6268a8b8dee1796
Vishal Rana <job.vishalrana@gmail.com>
2022-08-24 23:15:09 +0530

    added dagger dependency

--- app/src/main/java/com/rtech/diapplication/MainActivity.kt
+++ app/src/main/java/com/rtech/diapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.rtech.diapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rtech.diapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/rtech/diapplication/UserRegistrationService.kt
@@ -0,0 +1,8 @@
+package com.rtech.diapplication
+
+class UserRegistrationService {
+
+    fun registerUser(email: String, pass: String) {
+        // TODO : Impl req
+    }
+}
